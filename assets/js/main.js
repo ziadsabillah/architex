77cb3ed576d9627489ac48b5e1760a29a280ccb3
ZiaDoox <ziadoox.sabillah@gmail.com>
2021-04-23 17:53:08 +0000

    added git ignore

--- assets/js/main.js
+++ assets/js/main.js
@@ -125,7 +125,7 @@
   }, true)
 
   /**
-   * Scrool with ofset on links with a class name .scrollto
+   * Scrool with offset on links with a class name .scrollto
    */
   on('click', '.scrollto', function(e) {
     if (select(this.hash)) {
